1803a41f13d4ee092b3374c32c9033c777734856
crisipix <wongc5@gmail.com>
2017-04-19 23:32:19 -0400

    initialize project

---
+++ WebApiStarter/WebExample/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace WebExample.Controllers
+{
+
+    [RoutePrefix("api/Account")]
+    public class AccountController : ApiController
+    {
+        public AccountController()
+        { }
+
+        [HttpGet]
+        [Route("Main")]
+        public async Task<string> GetAccount()
+        {
+            return await Task.Run(() => { return "Test"; });
+        }
+    }
+}
---
+++ WebApiStarter/WebExample/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace WebExample
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            //SwaggerConfig.Register(GlobalConfiguration.Configuration);
+            /*
+             httpConfiguration
+     .EnableSwagger(c => c.SingleApiVersion("v1", "A title for your API"))
+     .EnableSwaggerUi();
+             */
+        }
+    }
+}
